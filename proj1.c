17ea759efee492254c413087828d646bd2ee4af4
Anusha Muhuri <anushabarbie7@gmail.com>
2026-01-08 22:32:23 +0530

    Committing

---
+++ proj1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int testMarks[10];
+    for(int i=0; i<10; i++){
+        testMarks[i]=-1;
+    }
+    for(int i=0;i<10;i++){
+        printf("Input marks %d: ",(i+1));
+        scanf("%d", &testMarks[i]);
+        if(testMarks[i]>100 || testMarks[i]<0){
+            printf("Invalid marks. Please re-enter.\n");
+            printf("Input marks %d: ",(i+1));
+            scanf("%d", &testMarks[i]);
+        }
+    }
+    float sum=0.0;
+    for(int i=0;i<10;i++){
+        sum=sum+(float)testMarks[i];
+    }
+    float avg=sum/10.0;
+    printf("Passed marks: ");
+    for(int i=0;i<10;i++){
+        if(testMarks[i]>avg){
+            printf("%d ",testMarks[i]);
+        }    
+    }
+    return 0;
+}
