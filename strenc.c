ee077f3663c82a3cc1176ee6631972876d4273d5
Anusha Muhuri <anushabarbie7@gmail.com>
2026-01-16 23:19:23 +0530

    changes made to string encoder program

--- strenc.c
+++ strenc.c
@@ -2,11 +2,13 @@
 #include<string.h>
 #include<stdlib.h>
 int main(){
-    printf("enter a string of alphabets in alphabetical order: ");
+    printf("enter a string of alphabets in alphabetical order(short hand): ");
     char str[20];
     scanf("%s",str);
     for(int i=0;i<strlen(str);i++){
-        str[i]=(char)((int)str[i]+3);
+        if(str[i]>='a'&&str[i<='z']){
+        str[i]='a'+(str[i]-'a'+3)%26;
+        }
     }
     printf("The result is: %s ", str);
     return 0;
