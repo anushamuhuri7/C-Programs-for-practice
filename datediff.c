1a16e5c950cee4b2f1d14b38e67a9b11ac4db045
Anusha Muhuri <anushabarbie7@gmail.com>
2026-01-18 23:54:36 +0530

    second commit

--- datediff.c
+++ datediff.c
@@ -2,7 +2,15 @@
 #include<stdlib.h>
 #include<string.h>
 struct date{
- char day[20];
- char month[20];
+ int day;
+ int month;
  int year;
 };
+int main(){
+    struct date d1;
+    struct date d2;
+    printf("Enter first date (dd/mm/yyyy) (with spaces in between): ");
+    scanf("%d %d %d", &d1.day, &d1.month, &d1.year);
+    printf("Enter second date (dd/mm/yyyy) (with spaces in between): ");
+    
+}
