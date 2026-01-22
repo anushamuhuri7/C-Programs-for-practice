bcad05453b47f247cecefd605d472b88f05d7761
Anusha Muhuri <anushabarbie7@gmail.com>
2026-01-22 23:47:43 +0530

    Third commit

--- parst.c
+++ parst.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdbool.h>
 #include<stdlib.h>
 #define MAX 100
@@ -12,17 +13,41 @@ bool bln(char *expr){
     char stack[MAX];
     int top=-1;
  for (int i = 0; i < strlen(expr); i++) {
-        char current = expr[i];
+        char curr = expr[i];
 
-        if (current == '{' || current == '(' || current == '[') {
+        if (curr == '{' || curr == '(' || curr == '[') {
             if (top >= MAX - 1) return false; 
-            stack[++top] = current;
+            stack[++top] = curr;
         }
-        else if (current == '}' || current == ')' || current == ']') {
-            if (top == -1 || !is_matching_pair(stack[top], current)) {
+        else if (curr == '}' || curr == ')' || curr == ']') {
+            if (top == -1 || !match(stack[top], curr)) {
                 return false;
             }
             top--;
         }
     }
+    return (top == -1);
+}
+int main(){
+    char in1[MAX];
+    char in2[MAX];
+    printf("Enter first sequence of brackets (don't close a bracket before it is opened): ");
+    scanf("%s", in1);
+    printf("Enter second sequence of brackets (don't close a bracket before it is opened): ");
+    scanf("%s", in2);
+    printf("Testing 1st one: \n");
+    if(bln(in1)){
+        printf("Output: Balanced\n");
+    }
+    else{
+        printf("Output: Unbalanced\n");
+    }
+    printf("Testing 2nd one: \n");
+    if(bln(in2)){
+        printf("Output: Balanced\n");
+    }
+    else{
+        printf("Output: Unbalanced\n");
+    }
+    return 0;
 }
