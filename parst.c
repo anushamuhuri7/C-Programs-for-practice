ea6fd8d19a0bcef0ee06df957dd7a3527ed0dbeb
Anusha Muhuri <anushabarbie7@gmail.com>
2026-01-22 23:25:39 +0530

    second commit

--- parst.c
+++ parst.c
@@ -8,3 +8,21 @@ bool match(char c1, char c2) {
     if (c1 == '[' && c2 == ']') return true;
     return false;
 }
+bool bln(char *expr){
+    char stack[MAX];
+    int top=-1;
+ for (int i = 0; i < strlen(expr); i++) {
+        char current = expr[i];
+
+        if (current == '{' || current == '(' || current == '[') {
+            if (top >= MAX - 1) return false; 
+            stack[++top] = current;
+        }
+        else if (current == '}' || current == ')' || current == ']') {
+            if (top == -1 || !is_matching_pair(stack[top], current)) {
+                return false;
+            }
+            top--;
+        }
+    }
+}
