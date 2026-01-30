e71cbbbb06f882832cc1766af0d2dbb56be90a9a
Anusha Muhuri <anushabarbie7@gmail.com>
2026-01-30 23:18:37 +0530

    new commit

--- pointers.c
+++ pointers.c
@@ -1,21 +1,40 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct LL{
-int item;
-struct LL *link;
+#include <stdio.h>
+#include <stdlib.h>
+
+struct LL {
+    int item;
+    struct LL *link;
 };
-struct LL *add_node_front(struct LL *fst,int ins){
-if(fst==NULL){
-struct LL *n1;
-n1=malloc(sizeof(struct LL));
-n1->item=ins;
-n1->link=NULL;
-printf("Node is added with value %d \n",ins );
-return n1;
+
+struct LL *add_node_front(struct LL *fst, int ins) {
+    
+    struct LL *n1 = malloc(sizeof(struct LL));
+    if (n1 == NULL) {
+        printf("Memory allocation failed!\n");
+        return fst;
+    }
+
+    n1->item = ins;
+    
+    // The new node's link should point to the current 'first' node
+    n1->link = fst; 
+    
+    printf("Node added with value %d\n", ins);
+    
+    // The new node is now the front of the list
+    return n1;
 }
-}
-int main(){
- struct LL *front;
-front=add_node_front(front,5);
-return 0;
+
+int main() {
+    // Crucial: Initialize to NULL
+    struct LL *front = NULL; 
+
+    // Adding nodes
+    front = add_node_front(front, 5);
+    front = add_node_front(front, 10);
+
+    // Let's print to verify
+    printf("Front item is: %d\n", front->item);
+
+    return 0;
 }
