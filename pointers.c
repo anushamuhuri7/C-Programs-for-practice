f37e1eee25334414ab4fb952afc7a8f406884558
Anusha Muhuri <anushabarbie7@gmail.com>
2026-02-01 21:43:08 +0530

    commit new

--- pointers.c
+++ pointers.c
@@ -6,35 +6,67 @@ struct LL {
     struct LL *link;
 };
 
-struct LL *add_node_front(struct LL *fst, int ins) {
+// Helper function to create a new node
+struct LL* create_node(int val) {
+    struct LL *newNode = malloc(sizeof(struct LL));
+    if (newNode != NULL) {
+        newNode->item = val;
+        newNode->link = NULL;
+    }
+    return newNode;
+}
+
+struct LL* add_at_pos(struct LL *fst, int ins, int pos) {
+    struct LL *newNode = create_node(ins);
     
-    struct LL *n1 = malloc(sizeof(struct LL));
-    if (n1 == NULL) {
-        printf("Memory allocation failed!\n");
+    // Case 1: Insert at the very beginning (Position 0)
+    if (pos == 0) {
+        newNode->link = fst;
+        return newNode;
+    }
+
+    struct LL *temp = fst;
+    // Move temp to the node at (pos - 1)
+    for (int i = 0; i < pos - 1 && temp != NULL; i++) {
+        temp = temp->link;
+    }
+
+    // Case 2: Position is out of bounds (beyond the end of the list)
+    if (temp == NULL) {
+        printf("Position out of range. Node not added.\n");
+        free(newNode);
         return fst;
     }
 
-    n1->item = ins;
+    // Case 3: Standard middle or end insertion
+    newNode->link = temp->link; // Link new node to the rest of the list
+    temp->link = newNode;       // Link previous node to the new node
     
-    // The new node's link should point to the current 'first' node
-    n1->link = fst; 
-    
-    printf("Node added with value %d\n", ins);
-    
-    // The new node is now the front of the list
-    return n1;
+    return fst;
+}
+
+void print_list(struct LL *node) {
+    while (node != NULL) {
+        printf("%d -> ", node->item);
+        node = node->link;
+    }
+    printf("NULL\n");
 }
 
 int main() {
-    // Crucial: Initialize to NULL
-    struct LL *front = NULL; 
+    struct LL *front = NULL;
 
-    // Adding nodes
-    front = add_node_front(front, 5);
-    front = add_node_front(front, 10);
+    // 1. Add to empty list (Position 0)
+    front = add_at_pos(front, 10, 0); 
+    // 2. Add to front (Position 0)
+    front = add_at_pos(front, 5, 0);  
+    // 3. Add to end (Position 2)
+    front = add_at_pos(front, 20, 2); 
+    // 4. Add to middle (Position 1)
+    front = add_at_pos(front, 15, 1); 
 
-    // Let's print to verify
-    printf("Front item is: %d\n", front->item);
+    printf("Final List: ");
+    print_list(front);
 
     return 0;
 }
