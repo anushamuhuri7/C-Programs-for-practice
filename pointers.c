cb95adc330a424a46ebd5e1fe58c232782de9eca
Anusha Muhuri <anushabarbie7@gmail.com>
2026-02-01 21:45:05 +0530

    some removals

--- pointers.c
+++ pointers.c
@@ -6,7 +6,7 @@ struct LL {
     struct LL *link;
 };
 
-// Helper function to create a new node
+
 struct LL* create_node(int val) {
     struct LL *newNode = malloc(sizeof(struct LL));
     if (newNode != NULL) {
@@ -19,28 +19,27 @@ struct LL* create_node(int val) {
 struct LL* add_at_pos(struct LL *fst, int ins, int pos) {
     struct LL *newNode = create_node(ins);
     
-    // Case 1: Insert at the very beginning (Position 0)
+   
     if (pos == 0) {
         newNode->link = fst;
         return newNode;
     }
 
     struct LL *temp = fst;
-    // Move temp to the node at (pos - 1)
+ 
     for (int i = 0; i < pos - 1 && temp != NULL; i++) {
         temp = temp->link;
     }
 
-    // Case 2: Position is out of bounds (beyond the end of the list)
+   
     if (temp == NULL) {
         printf("Position out of range. Node not added.\n");
         free(newNode);
         return fst;
     }
 
-    // Case 3: Standard middle or end insertion
-    newNode->link = temp->link; // Link new node to the rest of the list
-    temp->link = newNode;       // Link previous node to the new node
+    newNode->link = temp->link; 
+    temp->link = newNode;       
     
     return fst;
 }
@@ -56,13 +55,13 @@ void print_list(struct LL *node) {
 int main() {
     struct LL *front = NULL;
 
-    // 1. Add to empty list (Position 0)
+  
     front = add_at_pos(front, 10, 0); 
-    // 2. Add to front (Position 0)
+ 
     front = add_at_pos(front, 5, 0);  
-    // 3. Add to end (Position 2)
+
     front = add_at_pos(front, 20, 2); 
-    // 4. Add to middle (Position 1)
+
     front = add_at_pos(front, 15, 1); 
 
     printf("Final List: ");
