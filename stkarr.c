7921b85ccdbe628a9d1c04752b37170c04c75e9f
Anusha Muhuri <anushabarbie7@gmail.com>
2026-02-05 23:31:23 +0530

    new commit

--- stkarr.c
+++ stkarr.c
@@ -10,3 +10,23 @@ typedef struct {
     int capacity;
     int num_stacks;
 } MultiStack;
+
+
+void push(MultiStack* ms, int val, int sn) {
+    if (sn < 0 || sn >= ms->num_stacks) {
+        printf("Invalid stack number!\n");
+        return;
+    }
+    if (is_full(ms)) {
+        printf("Stack Overflow: No space left in the shared array!\n");
+        return;
+    }
+
+    int i = ms->free_top;
+    ms->free_top = ms->next[i];
+    ms->next[i] = ms->top[sn];
+    ms->top[sn] = i;
+    ms->data[i] = val;
+
+    printf("Pushed %d to Stack %d\n", val, sn);
+}
