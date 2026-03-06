1b5019d1f9e9e6e6dace7d24c620496fcd060c78
Anusha Muhuri <anushabarbie7@gmail.com>
2026-03-06 23:40:49 +0530

    changes made

--- delaft.c
+++ delaft.c
@@ -1,24 +1,28 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct linked_list
 {
-    int data ;
+    int data;
     struct linked_list *next;
-}node ;
+} node;
 
-node *temp,*ttemp,*p,*first,*q;
+node *temp, *ttemp, *p, *first, *q;
 void create_first(int value)
 {
-    first=(node*)malloc(sizeof (node));
+    first = (node *)malloc(sizeof(node));
     first->data = value;
-    first->next=NULL;
+    first->next = NULL;
 }
 void add_nodes(int value)
 {
-    temp=first;
-    while(temp->next!=NULL)
+    temp = first;
+    while (temp->next != NULL)
     {
         temp = temp->next;
     }
+    ttemp = (node *)malloc(sizeof(node));
+    ttemp->data = value;
+    ttemp->next = NULL;
+    temp->next = ttemp;
 }
