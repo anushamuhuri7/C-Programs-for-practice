4bcdc0b7c4f39d0ae8d7538b8320929f64b2a656
Anusha Muhuri <anushabarbie7@gmail.com>
2026-03-10 22:42:49 +0530

    Implement merge sort functions with merge and msort definitions

--- mgst.c
+++ mgst.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-void msort() {}
-void merge() {}
+void merge(int a[], int low, int mid, int high) {}
+void msort(int a[], int low, int high)
+{
+    if (low < high)
+    {
+        int mid = (low + high) / 2;
+        msort(a, low, mid);
+        msort(a, mid + 1, high);
+        merge(a, low, mid, high);
+    }
+}
 int main()
 {
     return 0;
