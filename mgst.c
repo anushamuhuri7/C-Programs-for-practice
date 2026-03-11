30c0ae9aa3ba2d5f3dc4942a6f68572b4e437503
Anusha Muhuri <anushabarbie7@gmail.com>
2026-03-11 19:04:25 +0530

    Fix memory allocation for merging arrays in merge function

--- mgst.c
+++ mgst.c
@@ -3,7 +3,21 @@
 void merge(int a[], int low, int mid, int high)
 {
     int n = high - low + 1, left = low, right = mid + 1, bArrIdx = 0, k;
-    int b = malloc(n * sizeof(int));
+    int *b = malloc(n * sizeof(int));
+    while (left <= mid && right <= high)
+    {
+        if (a[left] < a[right])
+            b[bArrIdx++] = a[left++];
+        else
+            b[bArrIdx++] = a[right++];
+    }
+    while (left <= mid)
+        b[bArrIdx++] = a[left++];
+    while (right <= high)
+        b[bArrIdx++] = a[right++];
+    for (k = 0; k < n; k++)
+        a[low + k] = b[k];
+    free(b);
 }
 void msort(int a[], int low, int high)
 {
