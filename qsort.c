26f1a62461f4e2a8358883311e7ff696e558bfa9
Anusha Muhuri <anushabarbie7@gmail.com>
2026-03-12 23:04:59 +0530

    quicksort program changed

--- qsort.c
+++ qsort.c
@@ -6,3 +6,19 @@ void swap(int *a, int *b)
     *a = *b;
     *b = temp;
 }
+void quick_sort(int a[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = a[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++)
+        {
+            if (a[j] < pivot)
+            {
+                i++;
+                swap(&a[i], &a[j]);
+            }
+        }
+    }
+}
