9b17d70416b748c1139118d76bec1d7d17303c1d
Anusha Muhuri <anushabarbie7@gmail.com>
2026-03-15 20:47:38 +0530

    Add output display for sorted array in main function

--- qsort.c
+++ qsort.c
@@ -45,5 +45,12 @@ int main()
     {
         scanf("%d", &a[i]);
     }
+    quick_sort(a, 0, n - 1);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
     return 0;
 }
