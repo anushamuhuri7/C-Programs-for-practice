084cc4eb270ecaa44f554bd6f49924230ad3f3af
Anusha Muhuri <anushabarbie7@gmail.com>
2026-03-18 21:24:06 +0530

    new commit2

--- beadst.c
+++ beadst.c
@@ -7,9 +7,16 @@ void bead_sort(int *a, int n)
     for (i = 0; i < n; i++)
         if (a[i] > max)
             max = a[i];
-}
-beads = (unsigned char *)calloc(max * n, sizeof(unsigned char));
-for (i = 0; i < n; i++)
-    for (j = 0; j < a[i]; j++)
-        beads[j * n + i] = 1;
+    beads = (unsigned char *)calloc(max * n, sizeof(unsigned char));
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < a[i]; j++)
+            beads[j * n + i] = 1;
+    }
+    for (j = 0; j < max; j++)
+    {
+        sum = 0;
+        for (i = 0; i < n; i++)
+            sum += beads[j * n + i];
+    }
 }
