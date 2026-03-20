ef0b52c8e69ab8511d84fe1ff7b38d196bda9519
Anusha Muhuri <anushabarbie7@gmail.com>
2026-03-20 23:17:24 +0530

    alter code

--- beadst.c
+++ beadst.c
@@ -1,37 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 void bead_sort(int *a, int n)
 {
-    int i, j, max = 0, sum;
-    unsigned char *beads;
+    int i, j, max = 0;
+
     for (i = 0; i < n; i++)
+    {
         if (a[i] > max)
             max = a[i];
-    beads = (unsigned char *)calloc(max * n, sizeof(unsigned char));
+    }
+    unsigned char *beads = (unsigned char *)calloc(max * n, sizeof(unsigned char));
+
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < a[i]; j++)
-            beads[j * n + i] = 1;
+        {
+            beads[i * max + j] = 1;
+        }
     }
     for (j = 0; j < max; j++)
     {
-        sum = 0;
+        int sum = 0;
         for (i = 0; i < n; i++)
-            sum += beads[j * n + i];
-        for (i = 0; i < sum; i++)
-            a[i] = a[i] + 1;
-        for (i = sum; i < n; i++)
-            a[i] = a[i] - 1;
+        {
+            sum += beads[i * max + j];
+            beads[i * max + j] = 0;
+        }
+
+        for (i = n - sum; i < n; i++)
+        {
+            beads[i * max + j] = 1;
+        }
     }
+
+    for (i = 0; i < n; i++)
+    {
+        int row_sum = 0;
+        for (j = 0; j < max; j++)
+        {
+            row_sum += beads[i * max + j];
+        }
+        a[i] = row_sum;
+    }
+
     free(beads);
 }
+
 int main()
 {
     int a[] = {5, 3, 1, 7, 4};
     int n = sizeof(a) / sizeof(a[0]);
+
     bead_sort(a, n);
+
+    printf("Sorted array: ");
     for (int i = 0; i < n; i++)
         printf("%d ", a[i]);
     printf("\n");
+
     return 0;
 }
