38791fa5478a713a0ade0efc73657f136bdc70d8
Anusha Muhuri <anushabarbie7@gmail.com>
2026-03-22 15:48:47 +0530

    binary tree added createnode

---
+++ bintree.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+struct node *createnode(int data)
+{
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    return newnode;
+}
