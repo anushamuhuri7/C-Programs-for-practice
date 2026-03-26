795e29e1e3810e8fdf3549a5f11652d8ab227421
Anusha Muhuri <anushabarbie7@gmail.com>
2026-03-26 17:35:04 +0530

    fixed lookup

--- binst.c
+++ binst.c
@@ -15,8 +15,8 @@ void lookup1(struct node *root, int target)
         printf("Found\n");
         return;
     }
-    lookup(root->left, target);
-    lookup(root->right, target);
+    lookup1(root->left, target);
+    lookup1(root->right, target);
 }
 void lookup2(struct node *root, int target)
 {
