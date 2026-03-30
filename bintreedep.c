9fe2a601a3aa90a999f90494561d85920e032f3d
Anusha Muhuri <anushabarbie7@gmail.com>
2026-03-30 23:37:47 +0530

    added newnode func

--- bintreedep.c
+++ bintreedep.c
@@ -6,3 +6,11 @@ struct node
     struct node *left;
     struct node *right;
 };
+struct node *newnode(int data)
+{
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
